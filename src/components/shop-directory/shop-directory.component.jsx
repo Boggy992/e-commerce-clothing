f80d6f62cbdfc0d9b317b39ea714899f8f9cfa89
Bogdan Zivanovic <b.zivanovic@vegait.rs>
2022-06-12 14:13:58 +0200

    Created categories in firestore and fetch data from

--- src/components/shop-directory/shop-directory.component.jsx
+++ src/components/shop-directory/shop-directory.component.jsx
@@ -1,21 +1,33 @@
-import React, { useContext } from "react";
+import React, { useContext, Fragment } from "react";
 
 import ShopCard from '../shop-card/shop-card.component';
 import { ProductsContext } from '../../contexts/products.context';
 
 import './shop-directory.style.scss';
+import { Link } from "react-router-dom";
 
 const ShopDirectory = () => {
     const { products } = useContext(ProductsContext);
 
     return (
-        <div className='shop-card__container'>
+        <>
             {
-                products.map(({ id, ...rest }) => (
-                    <ShopCard key={id} id={id} {...rest} />
-                ))
+                // Object.keys KREIRA NIZ OD KLJUCEVA (NEBROJIVIH KAO STO JE TITLE) [ hats, jackets, mens, sneakers, womens ]
+                Object.keys(products).map((title) =>
+                    <Fragment key={title}>
+                        <Link className="shop-page__link" to={`/shop/${title}`}>{title}</Link>
+                        <div className='shop-card__container'>
+                            {
+                                // products iako je objekat pomocu [title] nalazi koji je key-title u pitanju i mapira kroz sve elemente u npr. products[jackets] nizu
+                                products[title].filter((item, index) =>  index < 4).map(({ id, ...rest }) => (
+                                    <ShopCard key={id} id={id} {...rest} />
+                                ))
+                            }
+                        </div>
+                    </Fragment>
+                )
             }
-        </div>
+        </>
     )
 }
 
--- src/contexts/products.context.jsx
+++ src/contexts/products.context.jsx
@@ -4,24 +4,34 @@ import {
     useEffect
 } from 'react';
 
-import PRODUCTS from '../shop-data.json';
+import {
+    // addCollectionAndDocuments,
+    getCategoriesAndDocuments
+} from '../firebase/firebase.utils';
+
+// import PRODUCTS from '../shop-data.js';
 
 export const ProductsContext = createContext({
-    products: [],
-    setProducts: () => null
+    products: {}
 })
 
 export const ProductsProvider = ({ children }) => {
-    const [products, setProducts] = useState([])
-    const value = { products, setProducts }
+    const [products, setProducts] = useState({})
+    const value = { products }
+
+    // samo pokreni jednom da bi upisao podatke pod key-em 'categories' u firebase
+    // useEffect(() => {
+    //     addCollectionAndDocuments('categories', PRODUCTS)
+    // }, [])
 
     useEffect(() => {
-        if(!PRODUCTS) return
+        const getCategoriesMap = async () => {
+            const categoryMap = await getCategoriesAndDocuments()
+            setProducts(categoryMap)
+        }
 
-        const data = [...PRODUCTS]
-
-        setProducts(data)
+        getCategoriesMap()
     }, [])
 
     return <ProductsContext.Provider value={value}>{children}</ProductsContext.Provider>
-} 
+}
